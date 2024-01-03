37fd8a42da189c5bb3640c634a8387ac7952d59d
kadek.chresna <kadek.chresna@efishery.com>
2024-01-03 11:24:09 +0700

    init

--- connector-debezium-pgoutput.sh
+++ connector-debezium-pgoutput.sh
@@ -1,3 +1,15 @@
 curl -i -X DELETE localhost:8083/connectors/debezium-connect-1/
 
-curl -i -X GET localhost:8083/connectors/
+curl -i -X GET http://debezium--production.service.consul/connectors/
+
+
+curl -i -X GET http://debezium--production.service.consul/connectors/impostor-b2c-sailfish-feed_order_so_odoos-prod
+curl -i -X GET http://debezium-ui--production.service.consul/api/connectors/1
+
+
+
+curl -i -X GET http://debezium--preproduction.service.consul/connectors/impostor-b2c-sailfish-feed_order_so_odoos-uat
+
+
+curl -i -X GET http://debezium-ui--preproduction.service.consul/api/connectors/1
+
